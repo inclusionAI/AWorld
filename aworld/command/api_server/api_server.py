068089cec3f067d72ffcc4fb73b1a1a015ffb655
文龙 <wenlong.yanwl@antgroup.com>
2025-06-16 15:01:09 +0800

    api server

--- aworld/command/api_server/api_server.py
+++ aworld/command/api_server/api_server.py
@@ -1,3 +1,10 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+app.static_files("/static", "static")
+
 def run_api_server(port, args=None, **kwargs):
     print(f"Running API server on port {port}")
-     
+    uvicorn.run("api_server:app", host="0.0.0.0", port=port, reload=True)
