6f92ab28496dc54835da834119841e6cc108a06a
ganrunsheng <ganrunsheng@antgroup.com>
2025-09-09 11:42:49 +0800

    hide edit url in home page

--- docs/js/hide-home-edit.js
+++ docs/js/hide-home-edit.js
@@ -1,7 +1,7 @@
 // Check if the current page is the home page, if so hide the edit link
 if (window.location.pathname === '/' ||
     window.location.pathname.endsWith('/index.html') ||
-    window.location.pathname.endsWith('/inclusionai.github.io/AWorld/')) {
+    window.location.pathname.endsWith('inclusionai.github.io/AWorld/')) {
   const editLink = document.querySelector('.wy-breadcrumbs-aside a[href*="index.md"]');
   if (editLink) {
     editLink.style.display = 'none';
