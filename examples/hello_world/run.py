2bf7a1564eaa828a5b912afd068abd7768a0c378
ganrunsheng <ganrunsheng@antgroup.com>
2025-09-12 12:29:23 +0800

    add hello world example

--- examples/hello_world/run.py
+++ examples/hello_world/run.py
@@ -9,6 +9,7 @@ from aworld.core.tool.func_to_tool import be_tool
 from aworld.runner import Runners
 
 
+# as a local tool, not recommended in cluster mode
 @be_tool(tool_name='tool_1', tool_desc="tool description")
 def tool_1() -> str:
     return "hello world!"
@@ -17,7 +18,7 @@ def tool_1() -> str:
 model_config = ModelConfig(
     llm_provider=os.getenv("LLM_PROVIDER", "openai"),
     llm_model_name=os.getenv("LLM_MODEL_NAME", "gpt-4o"),
-    llm_base_url=os.getenv("LLM_BASE_URL", "http://localhost:34567"),
+    llm_base_url=os.getenv("LLM_BASE_URL", "your base url"),
     llm_api_key=os.getenv("LLM_API_KEY", "your key"),
     llm_temperature=os.getenv("LLM_TEMPERATURE", 0.0)
 )
@@ -27,20 +28,22 @@ agent_config = AgentConfig(
 
 agent1 = Agent(name="tool_agent",
                conf=agent_config,
-               system_prompt=""" You are a helpful agent, and must use search_api once.""",
+               system_prompt="""You are a helpful agent, and must use tool_1 once.""",
                tool_names=['tool_1'])
 
 agent = Agent(name="my_agent",
               conf=agent_config,
-              system_prompt=""" You are a helpful agent, and must use tool agent once.""",
+              system_prompt="""You are a helpful agent, and must use tool agent once.""",
               agent_names=[agent1.id()])
+
+swarm = Swarm(agent, register_agents=[agent1])
 
 if __name__ == "__main__":
     # run use the same process
-    # res = Runners.sync_run(input="use tool agent say", swarm=Swarm(agent, register_agents=[agent1]))
+    # res = Runners.sync_run(input="use tool agent say", swarm=swarm)
 
-    # run use the new process
-    res = Runners.sync_run(input="use tool agent say", swarm=Swarm(agent, register_agents=[agent1]),
+    # run use the multiprocess (new process)
+    res = Runners.sync_run(input="use tool agent say", swarm=swarm,
                            run_conf=RunConfig(engine_name=EngineName.LOCAL, reuse_process=False))
     # hello world
     print(res.answer)
--- examples/hello_world/run_on_spark.py
+++ examples/hello_world/run_on_spark.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 # Copyright (c) 2025 inclusionAI.
 from aworld.config import RunConfig, EngineName
-from aworld.core.agent.swarm import Swarm
 from aworld.runner import Runners
-from examples.multi_agents.collaborative.simple_demo.run import agent, agent1
+from examples.hello_world.run import swarm
 
 if __name__ == "__main__":
-    res = Runners.sync_run(input="use tool agent say", swarm=Swarm(agent, register_agents=[agent1]),
+    res = Runners.sync_run(input="use tool agent say", swarm=swarm,
                            run_conf=RunConfig(engine_name=EngineName.SPARK, in_local=True))
     # hello world
     print(res.answer)
