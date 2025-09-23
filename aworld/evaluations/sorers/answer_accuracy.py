cf4336ed2c0e5b731fe195caeda86dfcce305357
道问 <aohui.xah@antgroup.com>
2025-09-23 16:44:53 +0800

    :sparkles: optimization

--- aworld/evaluations/sorers/answer_accuracy.py
+++ aworld/evaluations/sorers/answer_accuracy.py
@@ -1,6 +1,8 @@
 from aworld.evaluations.base import Scorer
 from typing import Dict, Any
 
+from aworld.logs.util import logger
+
 
 class AnswerAccuracyScorer(Scorer):
 
@@ -10,6 +12,7 @@ class AnswerAccuracyScorer(Scorer):
         self.answer_column = answer_column
 
     async def score(self, index: int, input: dict, output: dict) -> float:
+        logger.info(f"score index: {index}, input: {input}, output: {output}")
         answer_except = input[self.answer_column]
         answer_actual = output.get(self.answer_column, '')
         return 1.0 if answer_except.strip() == answer_actual.strip() else 0.0
