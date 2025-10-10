65d458fe1f36a82d1d9590d22f651eede5e6af25
ganrunsheng <ganrunsheng@antgroup.com>
2025-10-10 18:46:39 +0800

    improve event runner; remove unused code

--- aworld/utils/async_func.py
+++ aworld/utils/async_func.py
@@ -5,8 +5,6 @@ import asyncio
 import contextlib
 from collections.abc import Generator
 from concurrent.futures import Future, ThreadPoolExecutor, as_completed
-from functools import wraps
-from typing import Callable, Optional, Union, Any, Dict
 
 
 @contextlib.contextmanager
@@ -26,66 +24,19 @@ def loop_in_new_thread() -> Generator[asyncio.AbstractEventLoop]:
                 try:
                     yield loop
                 finally:
-                    loop.call_soon_threadsafe(stop_event.set)
+                    loop.call_soon_threadsafe(stop_event.set, )
             else:
                 future.result()
 
 
-class Functionable:
-    def __init__(self, function: Callable[..., Any], *args: Any, **kwargs: Dict[str, Any]) -> None:
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.done: bool = False
-        self.error: bool = False
-        self.result: Optional[Any] = None
-        self.exception: Optional[Exception] = None
-
-    def __call__(self) -> None:
-        try:
-            self.result = self.function(*self.args, **self.kwargs)
-        except Exception as e:
-            self.error = True
-            self.exception = e
-        self.done = True
-
-    def call(self):
-        self.__call__()
+def start_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
 
 
-def async_decorator(*func, delay: Optional[Union[int, float]] = 0.5) -> Callable:
-    def wrapper(function: Callable[..., Any]) -> Callable[..., Any]:
-        @wraps(function)
-        async def inner_wrapper(*args: Any, **kwargs: Any) -> Any:
-            sleep_time = 0 if delay is None else delay
-            task = Functionable(function, *args, **kwargs)
-            # TODO: Use thread pool to process task
-            task.call()
-            if task.error:
-                raise task.exception
-            await asyncio.sleep(sleep_time)
-            return task.result
+def shutdown_loop(loop):
+    loop.stop()
 
-        return inner_wrapper
 
-    if not func:
-        return wrapper
-    else:
-        if asyncio.iscoroutinefunction(func[0]):
-            # coroutine function, return itself
-            return func[0]
-        return wrapper(func[0])
-
-def async_func(function: Callable[..., Any]) -> Callable[..., Any]:
-    @wraps(function)
-    async def inner_wrapper(*args: Any, **kwargs: Any) -> Any:
-        task = Functionable(function, *args, **kwargs)
-        task.call()
-        if task.error:
-            raise task.exception
-        return task.result
-
-    if asyncio.iscoroutinefunction(function):
-        # coroutine function, return itself
-        return function
-    return inner_wrapper
+def shutdown_all(loops):
+    [loop.call_soon_threadsafe(shutdown_loop, loop) for loop in loops]
