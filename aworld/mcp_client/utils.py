64bbc7180176e5f64f319548854ddbcea51fd64d
励峰 <honglifeng.hlf@antgroup.com>
2025-11-03 16:52:47 +0800

    session

--- aworld/mcp_client/utils.py
+++ aworld/mcp_client/utils.py
@@ -510,7 +510,8 @@ async def mcp_tool_desc_transform_v2(
                     params = server_config["params"].copy()
                     headers = params.get("headers") or {}
                     if context and context.session_id:
-                        headers["SESSION_ID"] = context.session_id
+                        #headers["SESSION_ID"] = context.session_id
+                        headers["SESSION_ID"] = f"{server_config['name']}_{context.session_id}"
                     if context and context.user:
                         headers["USER_ID"] = context.user
                     params["headers"] = headers
@@ -900,7 +901,7 @@ async def get_server_instance(
         elif "sse" == server_config.get("type", ""):
             headers = server_config.get("headers") or {}
             if context and context.session_id:
-                headers["SESSION_ID"] = context.session_id
+                headers["SESSION_ID"] = f"{server_name}_{context.session_id}"
             if context and context.user:
                 headers["USER_ID"] = context.user
             server = MCPServerSse(
@@ -919,7 +920,8 @@ async def get_server_instance(
         elif "streamable-http" == server_config.get("type", ""):
             headers = server_config.get("headers") or {}
             if context and context.session_id:
-                headers["SESSION_ID"] = context.session_id
+                headers["SESSION_ID"] = f"{server_name}_{context.session_id}"
+                #headers["SESSION_ID"] = context.session_id
             if context and context.user:
                 headers["USER_ID"] = context.user
             server = MCPServerStreamableHttp(
