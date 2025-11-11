f978e6e94c35e0a9e38e4cae36119f1ab15db6b9
劳谦 <huanggaochi.hgc@antgroup.com>
2025-11-11 17:35:40 +0800

    not use test ip pool

--- train/examples/train_gaia_with_aworld_verl/env/ip_pool.py
+++ train/examples/train_gaia_with_aworld_verl/env/ip_pool.py
@@ -1,18 +1,17 @@
-import logging
 import os
-import random
-import traceback
 
 import requests
+import logging
+import traceback
 
 logger = logging.getLogger(__name__)
 
 async def get_proxy_server():
-    api = f"{os.getenv('IP_POOL_PROXY')}/get_third_proxy?international=0"
+    api = f"{os.getenv('IP_POOL_PROXY')}/get_cn_proxy?interval=0&protocol=HTTP"
     try:
         response = requests.get(api)
         j = response.json()
-        p = random.choice(j["result"]["data"])
+        p = j["result"]["data"]
         proxy = f"{p['proxy_public_ip']}:{p['proxy_port']}"
         return proxy
     except:
