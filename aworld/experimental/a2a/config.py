051419d73622f34363689f40f107216a8a73fd74
chunfeng.w <chunfeng.w@antgroup.com>
2025-11-12 11:32:49 +0800

    [a2a] a2a client proxy

--- aworld/experimental/a2a/config.py
+++ aworld/experimental/a2a/config.py
@@ -4,7 +4,10 @@ from typing import Type, Union
 
 from a2a.server.apps import A2AFastAPIApplication, A2AStarletteApplication
 from a2a.server.tasks import PushNotificationConfigStore, TaskStore, BasePushNotificationSender, InMemoryTaskStore
-from a2a.types import AgentSkill
+from a2a.types import AgentSkill, TransportProtocol, PushNotificationConfig
+from a2a.client.optionals import Channel
+from a2a.client.client import Consumer
+from collections.abc import Callable
 
 from aworld.config import BaseConfig
 
@@ -31,3 +34,18 @@ class ServingConfig(BaseConfig):
     server_app: str = "fastapi"  # Options: "fastapi", "starlette" or "grpc"
     uvicorn_config: dict = {}
     version: str = "0.0.1"
+
+
+class ClientConfig(BaseConfig):
+    streaming: bool = False
+    # Whether client prefers to poll for updates from message:send. It is the callers job to check if the response is completed and if not run a polling loop.
+    polling = False
+    timeout: float = 600.0
+    supported_transports: list[TransportProtocol] = [TransportProtocol.JSONRPC]
+    grpc_channel_factory: Callable[[str], Channel] | None = None
+    # Whether to use client transport preferences over server preferences. Recommended to use server preferences in most situations.
+    use_client_preference: bool = False
+    accepted_output_modes: list[str] = []
+    push_notification_configs: list[PushNotificationConfig] = []
+    consumers: list[Consumer] = []
+
