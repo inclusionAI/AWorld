2b92c07eede542baceadcd61ec7fae9100f7750e
wangchunf <chunfeng.w@antgroup.com>
2025-11-17 16:43:07 +0800

    [trace] span  rename (#582)

--- aworld/trace/__init__.py
+++ aworld/trace/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2025 inclusionAI.
 from aworld.trace.context_manager import TraceManager
 from aworld.trace.constants import (
+    SPAN_NAME_PREFIX_EVENT,
     SPAN_NAME_PREFIX_EVENT_AGENT,
     SPAN_NAME_PREFIX_EVENT_TOOL,
     SPAN_NAME_PREFIX_EVENT_TASK,
@@ -23,9 +24,9 @@ from typing import Callable, Any
 
 def get_span_name_from_message(message: 'aworld.core.event.base.Message') -> tuple[str, RunType]:
     from aworld.core.event.base import Constants
-    span_name = (message.receiver or message.id)
     if message.category == Constants.AGENT:
-        return (SPAN_NAME_PREFIX_EVENT_AGENT + span_name, RunType.AGNET)
+        receiver = message.receiver or message.id
+        return (SPAN_NAME_PREFIX_EVENT_AGENT + receiver, RunType.AGNET)
     if message.category == Constants.TOOL:
         action = message.payload
         if isinstance(action, (list, tuple)):
@@ -33,15 +34,23 @@ def get_span_name_from_message(message: 'aworld.core.event.base.Message') -> tup
         if action:
             tool_name, run_type = get_tool_name(action.tool_name, action)
             return (SPAN_NAME_PREFIX_EVENT_TOOL + tool_name, run_type)
-        return (SPAN_NAME_PREFIX_EVENT_TOOL + span_name, RunType.TOOL)
+        return (SPAN_NAME_PREFIX_EVENT_TOOL, RunType.TOOL)
     if message.category == Constants.TASK:
         if message.topic:
-            return (SPAN_NAME_PREFIX_EVENT_TASK + message.topic + "_" + span_name, RunType.OTHER)
+            return (SPAN_NAME_PREFIX_EVENT_TASK + message.topic, RunType.OTHER)
         else:
-            return (SPAN_NAME_PREFIX_EVENT_TASK + span_name, RunType.OTHER)
+            return (SPAN_NAME_PREFIX_EVENT_TASK, RunType.OTHER)
     if message.category == Constants.OUTPUT:
-        return (SPAN_NAME_PREFIX_EVENT_OUTPUT + span_name, RunType.OTHER)
-    return (SPAN_NAME_PREFIX_EVENT_OTHER + span_name, RunType.OTHER)
+        output_type = message.payload.output_type()
+        if output_type:
+            if output_type == "step" and hasattr(message.payload, "status"):
+                status = message.payload.status
+                return (SPAN_NAME_PREFIX_EVENT_OUTPUT + output_type + "." + status, RunType.OTHER)
+            return (SPAN_NAME_PREFIX_EVENT_OUTPUT + output_type, RunType.OTHER)
+        return (SPAN_NAME_PREFIX_EVENT_OUTPUT, RunType.OTHER)
+    if message.category:
+        return (SPAN_NAME_PREFIX_EVENT + message.category, RunType.OTHER)
+    return (SPAN_NAME_PREFIX_EVENT_OTHER, RunType.OTHER)
 
 
 def message_span(message: 'aworld.core.event.base.Message' = None, attributes: dict = None):
@@ -54,6 +63,7 @@ def message_span(message: 'aworld.core.event.base.Message' = None, attributes: d
             "event.sender": message.sender or "",
             "event.category": message.category,
             "event.id": message.id,
+            "event.header": str(message.headers),
             semconv.SESSION_ID: message.session_id
         }
         message_span_attribute.update(attributes or {})
@@ -73,13 +83,19 @@ def handler_span(message: 'aworld.core.event.base.Message' = None, handler: Call
     if message:
         run_type = RunType.OTHER
         if message.category == Constants.AGENT:
-            span_name = SPAN_NAME_PREFIX_AGENT + span_name
             run_type = RunType.AGNET
             attributes.update(get_agent_span_attributes(handler.__self__, message))
+            if attributes.get(semconv.AGENT_NAME):
+                span_name = SPAN_NAME_PREFIX_AGENT + attributes.get(semconv.AGENT_NAME) + "." + span_name
+            else:
+                span_name = SPAN_NAME_PREFIX_AGENT + span_name
         if message.category == Constants.TOOL:
-            span_name = SPAN_NAME_PREFIX_TOOL + span_name
             run_type = RunType.TOOL
             attributes.update(get_tool_span_attributes(handler.__self__, message))
+            if attributes.get(semconv.TOOL_NAME):
+                span_name = SPAN_NAME_PREFIX_TOOL + attributes.get(semconv.TOOL_NAME) + "." + span_name
+            else:
+                span_name = SPAN_NAME_PREFIX_TOOL + span_name
             if attributes.get(ATTRIBUTES_MESSAGE_RUN_TYPE_KEY):
                 run_type = RunType[attributes.get(ATTRIBUTES_MESSAGE_RUN_TYPE_KEY)]
         return GLOBAL_TRACE_MANAGER.span(
