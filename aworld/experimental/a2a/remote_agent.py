146c6abadde6a20eeb24da954ca97bb45f461770
chunfeng.w <chunfeng.w@antgroup.com>
2025-11-18 14:31:19 +0800

    [a2a] remote agent

--- aworld/experimental/a2a/remote_agent.py
+++ aworld/experimental/a2a/remote_agent.py
@@ -7,10 +7,10 @@ from a2a.types import (
 )
 from aworld.core.agent.base import BaseAgent
 from aworld.core.common import ActionResult, Observation, ActionModel, Config, TaskItem
-from aworld.core.event.base import Message
+from aworld.core.event.base import Message, AgentMessage
 from aworld.core.task import Task, TaskResponse
 from aworld.logs.util import logger
-from aworld.config.conf import ConfigDict
+from aworld.config.conf import ConfigDict, AgentConfig
 from aworld.utils.common import sync_exec, convert_to_snake
 from aworld.experimental.a2a.client_proxy import A2AClientProxy
 from aworld.experimental.a2a.config import ClientConfig
@@ -24,9 +24,12 @@ class RemoteAgent(BaseAgent):
                  agent_id: str = None,
                  *,
                  task: Any = None,
+                 tool_names: List[str] = None,
                  **kwargs,):
-        self.name = name
+        self._name = name if name else convert_to_snake(self.__class__.__name__)
         self.conf = conf
+        if not self.conf:
+            self.conf = ConfigDict(AgentConfig().model_dump())
         self._name = name if name else convert_to_snake(self.__class__.__name__)
         self._desc = desc if desc else self._name
         self._id = (
@@ -34,6 +37,15 @@ class RemoteAgent(BaseAgent):
         )
         self.task: Any = task
         self.context = kwargs.get("context", None)
+        self.tool_names: List[str] = tool_names or []
+
+    def _update_headers(self, input_message: Message) -> Dict[str, Any]:
+        headers = input_message.headers.copy()
+        headers['context'] = input_message.context
+        headers['level'] = headers.get('level', 0) + 1
+        if input_message.group_id:
+            headers['parent_group_id'] = input_message.group_id
+        return headers
 
     def policy(self, observation: Observation, info: Dict[str, Any] = {}, message: Message = None, **kwargs) -> List[
             ActionModel]:
@@ -48,8 +60,19 @@ class RemoteAgent(BaseAgent):
         remote_task = Task(input=observation.content)
 
         response_task = await self.call_agent(remote_task, info, **kwargs)
+        logger.info(f"Agent{type(self)}#{self.id()}: async_policy end, response_task: {response_task}")
         result = ActionModel(agent_name=self.id(), policy_info=response_task.answer)
+        self._finished = True
         return [result]
+
+    async def async_post_run(self, policy_result: List[ActionModel], policy_input: Observation,
+                             message: Message = None) -> Message:
+        caller = policy_input.from_agent_name if policy_input.from_agent_name else policy_input.observer
+        return AgentMessage(payload=policy_result,
+                            caller=caller,
+                            sender=self.id(),
+                            session_id=message.context.session_id if message.context else "",
+                            headers=self._update_headers(message))
 
     @abc.abstractmethod
     async def call_agent(self, request_task: Task, info: Dict[str, Any] = {}, **kwargs) -> TaskResponse:
