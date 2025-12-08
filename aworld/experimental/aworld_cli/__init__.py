ed8555dd39c2d7bf254dbecdf83be8c2942a1fbc
道问 <aohui.xah@antgroup.com>
2025-12-08 15:57:30 +0800

    :tada: add support client

---
+++ aworld/experimental/aworld_cli/__init__.py
@@ -0,0 +1,22 @@
+"""
+AWorld CLI package.
+"""
+import os
+os.environ['AWORLD_DISABLE_CONSOLE_LOG'] = 'true'
+
+from .console import AWorldCLI
+from .runtime import LocalRuntime, RemoteRuntime, MixedRuntime, BaseAgentRuntime
+from .models import AgentInfo, TeamInfo
+from .executors import AgentExecutor
+
+__all__ = [
+    "AWorldCLI",
+    "LocalRuntime",
+    "RemoteRuntime",
+    "MixedRuntime",
+    "BaseAgentRuntime",
+    "AgentInfo",
+    "TeamInfo",
+    "AgentExecutor",
+]
+
