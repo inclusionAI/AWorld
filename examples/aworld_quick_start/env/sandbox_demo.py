5d33bed82ced8a9220310b5484a2f1d87da02ed3
励峰 <honglifeng.hlf@antgroup.com>
2026-01-05 15:27:23 +0800

    env client demo

--- examples/aworld_quick_start/env/sandbox_demo.py
+++ examples/aworld_quick_start/env/sandbox_demo.py
@@ -21,6 +21,22 @@ async def main():
     result = await Sandbox.register(servers=servers)
 
     # 2、call the environment
+    #1、注册中心，通过环境变量获取注册中心地址，如果不填写默认本地，默认注册中心本地地址是：~/workspace/registry.json
+    # os.environ["ENV_REGISTRY_URL"] = '**'
+    # os.environ["REGISTRY_TOKEN"] = '**'
+    # servers = {
+    #     "filesystem": {
+    #         "command": "npx",
+    #         "args": [
+    #             "-y",
+    #             "@modelcontextprotocol/server-filesystem",
+    #             "~/tmp"
+    #         ]
+    #     }
+    # }
+    # result = await Sandbox.register(servers=servers)
+    #
+    # 2、使用注册中心调用环境
     sand_box = Sandbox(
         tools=["read_text_file"],
         mcp_servers=["filesystem"])
@@ -32,6 +48,21 @@ async def main():
     # await sand_box.call_tool(...)
 
     # 3、custom config
+    result = await sand_box.call_tool(action_list=[
+        {
+
+            "tool_name": "filesystem",
+            "action_name": "read_text_file",
+            "params": {
+                "path": "/Users/honglifeng/tmp/test.py"
+
+            }
+        }
+    ])
+    print(result)
+    return
+    #
+    # 3、使用自定义配置使用环境
     sand_box = Sandbox(
         mcp_servers=["filesystem"],
         mcp_config={
