63335ac6de48946679f32a9eac53f7093b0c4448
rainsonGain <ganrunsheng@antgroup.com>
2026-01-20 09:14:11 +0800

    Feature/evolve task (#713)
    * add output to UI;
    add evolve plan render
    
    * fix async func
    
    * fix params
    
    * render log
    
    * update prompt

--- train/evolve/evolve_agent.py
+++ train/evolve/evolve_agent.py
@@ -6,6 +6,7 @@ from aworld.core.agent.base import BaseAgent
 from aworld.core.common import Observation, ActionModel
 from aworld.core.event.base import Message
 from aworld.experimental.aworld_cli.core import agent
+from aworld.logs.util import logger
 from aworld.runners.utils import execute_runner
 from train.evolve.config import EvolutionConfig
 from train.evolve.evolution_runner import EvolutionRunner
@@ -18,9 +19,16 @@ class EvolveAgent(BaseAgent):
 
     async def async_policy(self, observation: Observation, info: Dict[str, Any] = {}, message: Message = None,
                            **kwargs) -> List[ActionModel]:
-        task = observation.content
+        logger.info(f"Task: {observation.content} auto evolve starting...")
+
+        task = message.context.get_task()
+        if not task:
+            task = observation.content
+
         evolve_conf = EvolutionConfig()
+        # Pipeline that can be used independently
         runner = EvolutionRunner(task=task, config=evolve_conf)
-        res = await execute_runner([runner], evolve_conf.run_conf)
+        res = await execute_runner(runners=runner, run_conf=evolve_conf.run_conf)
         res = res.get("0")
+        logger.info(f"Task: {observation.content} auto evolve finished.")
         return [ActionModel(agent_name=self.id(), policy_info=res.answer)]
