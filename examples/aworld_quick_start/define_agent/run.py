feb4237adc04c5a91e158002faf77438af338ebf
ganrunsheng <ganrunsheng@antgroup.com>
2026-01-22 17:56:45 +0800

    example reset logger

--- examples/aworld_quick_start/define_agent/run.py
+++ examples/aworld_quick_start/define_agent/run.py
@@ -6,10 +6,17 @@ import os
 os.environ['AWORLD_LOG_PATH'] = '/tmp/aworld_logs'
 
 from aworld.agents.llm_agent import Agent
+from aworld.logs.util import logger
 from aworld.runner import Runners
 from examples.aworld_quick_start.common import agent_config
 
 async def main():
+    # reset show DEBUG log level
+    logger.reset_level("DEBUG")
+    # reset log format, one line log style
+    logger.reset_format("<black>{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | \
+{extra[name]} PID: {process}, TID:{thread} |</black> <bold>{name}.{function}:{line}</bold> \
+|| <level>{message}</level> {exception}")
     # Define an agent
     demo = Agent(
         conf=agent_config,
