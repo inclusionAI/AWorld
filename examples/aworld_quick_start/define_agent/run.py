dca1ebca3f772862f36da156c3e7543ce5c67aa1
rainsonGain <ganrunsheng@antgroup.com>
2026-01-22 18:20:09 +0800

    custom log format and level (#718)
    * custom log format and level
    
    * example reset logger
    
    * trace id set

--- examples/aworld_quick_start/define_agent/run.py
+++ examples/aworld_quick_start/define_agent/run.py
@@ -2,14 +2,29 @@
 # Copyright (c) 2025 inclusionAI.
 import asyncio
 import os
+
+from aworld import trace
+from aworld.core.task import Task
+from aworld.trace import ObservabilityConfig
+
 # use custom log path
 os.environ['AWORLD_LOG_PATH'] = '/tmp/aworld_logs'
 
 from aworld.agents.llm_agent import Agent
+from aworld.logs.util import logger
 from aworld.runner import Runners
 from examples.aworld_quick_start.common import agent_config
 
+trace.configure(ObservabilityConfig())
+
+
 async def main():
+    # reset show DEBUG log level
+    logger.reset_level("DEBUG")
+    # reset log format, one line log style
+    logger.reset_format("<black>{time:YYYY-MM-DD HH:mm:ss.SSS}/ {extra[trace_id]} | {level} | \
+{extra[name]} PID: {process}, TID:{thread} |</black> <bold>{name}.{function}:{line}</bold> \
+|| <level>{message}</level> {exception}")
     # Define an agent
     demo = Agent(
         conf=agent_config,
@@ -19,10 +34,11 @@ async def main():
         # agent_prompt="for fine-tune",
         # agent_id="for special fix agent id",
     )
-
+    # custom trace id
+    task = Task(input="who are you?", agent=demo, trace_id="abcd")
     # Run the agent
-    res = await Runners.run(input="who are you?", agent=demo)
-    print(res.answer)
+    res = await Runners.run_task(task=task)
+    print(res.get(task.id).answer)
 
 
 if __name__ == '__main__':
