64740afc3254134c853d64e0bfd14adfa219b120
道问 <aohui.xah@antgroup.com>
2026-02-03 11:42:13 +0800

    remove langextract

--- aworld/core/context/amni/processor/op/__init__.py
+++ aworld/core/context/amni/processor/op/__init__.py
@@ -2,14 +2,10 @@
 op module - contains all operation related classes
 """
 from .base import BaseOp, MemoryCommand
-from .langextract_op import LangExtractOp
 from .system_prompt_augment_op import SystemPromptAugmentOp
 from .op_factory import OpFactory, memory_op
 from .save_memory_op import SaveMemoryOp
 from .tool_result_process_op import ToolResultOffloadOp
-from .extract_agent_experience_op import AgentExperienceLangExtractOp
-from .extract_tool_fact_op import ExtractToolFactOp
-from .extract_user_profile_op import UserProfileLangExtractOp
 
 __all__ = [
     # factory and decorator
@@ -21,10 +17,6 @@ __all__ = [
     # components
     "SaveMemoryOp",
     "SystemPromptAugmentOp",
-    "LangExtractOp",
-    "UserProfileLangExtractOp",
-    "AgentExperienceLangExtractOp",
-    "ToolResultOffloadOp",
-    "ExtractToolFactOp"
+    "ToolResultOffloadOp"
 ]
 
