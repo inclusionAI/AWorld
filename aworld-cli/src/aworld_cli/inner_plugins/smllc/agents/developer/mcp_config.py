bff36190cecba9c4ee518d6dd207d7b16058436e
劳谦 <huanggaochi.hgc@antgroup.com>
2026-02-26 13:40:11 +0800

    [cli]: develop, evaluator

---
+++ aworld-cli/src/aworld_cli/inner_plugins/smllc/agents/developer/mcp_config.py
@@ -0,0 +1,13 @@
+mcp_config = {
+    "mcpServers": {
+        "terminal": {
+            "command": "python",
+            "args": [
+                "-m",
+                "examples.gaia.mcp_collections.tools.terminal"
+            ],
+            "env": {},
+            "client_session_timeout_seconds": 9999.0
+        }
+    }
+}
