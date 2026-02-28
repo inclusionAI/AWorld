fc79be9a1ae9d161760d234eaf4a74950d2b2965
劳谦 <huanggaochi.hgc@antgroup.com>
2026-02-28 17:04:13 +0800

    [cli]: ctrl c to exit

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,5 +1,17 @@
 # coding: utf-8
 # Copyright (c) 2025 inclusionAI.
+import sys
+
+
+def _suppress_keyboard_interrupt_traceback(exc_type, exc_value, exc_tb):
+    """Suppress KeyboardInterrupt traceback; exit cleanly."""
+    if exc_type is KeyboardInterrupt:
+        sys.exit(0)
+    sys.__excepthook__(exc_type, exc_value, exc_tb)
+
+
+sys.excepthook = _suppress_keyboard_interrupt_traceback
+
 from dotenv import load_dotenv
 
 load_dotenv()
