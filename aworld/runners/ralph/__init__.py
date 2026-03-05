e0319d039bf7a09bbec3502c3190cf80cadbfab3
rainsonGain <ganrunsheng@antgroup.com>
2026-03-05 14:11:00 +0800

    Long-term run initial (#804)

---
+++ aworld/runners/ralph/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Copyright (c) inclusionAI.
+
+import os
+
+from aworld.evaluations import _auto_discover_scorers
+
+# Auto-discover validation scorers
+_auto_discover_scorers(
+    current_dir=os.path.join(os.path.dirname(__file__), 'validate'),
+    package_name=f'{__name__}.validate'
+)
